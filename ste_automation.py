3ba4d2143c63f927c371d3127408d758dbb43a90
Guilherme Iablonovski <guilhermeiablo@gmail.com>
2019-06-02 19:25:02 -0300

    3

--- ste_automation.py
+++ ste_automation.py
@@ -5,7 +5,7 @@ from time import sleep
 usr = input('Enter your username or email id: ')
 pwd = getpass('Enter your password : ')
 
-driver = webdriver.Safari()
+driver = webdriver.Chrome()
 driver.get('https://stesa.inventsys.com.br/10762/item/list')
 
 username_box = driver.find_element_by_name('username_email')
@@ -24,9 +24,5 @@ export_btn.click()
 
 sleep(5)
 
-csv_btn = driver.find_element_by_xpath("//input[@value='csv']")
-csv_btn.submit()
 
-export_csv = driver.find_element_by_name('submit')
-export_csv.submit()
 
